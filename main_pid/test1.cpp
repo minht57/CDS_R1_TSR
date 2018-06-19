afc28f9b5c6077336169e089a14c71ba07f17371
minht57 <minhtri1996vl@gmail.com>
2018-06-19 07:58:58 +0700

    Clean project

--- main_pid/test1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <thread>
-
-int main(int argc, char ** argv)
-{
-    while(1);
-}
--- main_pid/test_OpenMP.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <thread>
-
-int main(int argc, char ** argv)
-{
-    unsigned numCPU = std::thread::hardware_concurrency();
-    std::cout << numCPU << std::endl;
-    while(1);
-}
